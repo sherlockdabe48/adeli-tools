8b0addc3194f8cc980d0861b1879da28e00fb984
Poramin Lueanskun <79498600+sherlockdabe48@users.noreply.github.com>
2023-10-15 19:10:45 +0800

    Create booleans.py

---
+++ booleans.py
@@ -0,0 +1,8 @@
+def can_run_for_president(age, is_natural_born_citizen):
+    """Can someone of the given age and citizenship status run for president in the US?"""
+    # The US Constitution says you must be a natural born citizen *and* at least 35 years old
+    return is_natural_born_citizen and (age >= 35)
+
+print(can_run_for_president(19, True))
+print(can_run_for_president(55, False))
+print(can_run_for_president(55, True))
